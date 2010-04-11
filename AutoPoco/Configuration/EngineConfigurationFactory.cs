c78e5457196c9707efb321952d83f1afcad15c45
rashton <rashton@localhost>
2010-04-11 16:41:58 +0100

    Further refactored the configuration factory for less code-per-method

--- AutoPoco/Configuration/EngineConfigurationFactory.cs
+++ AutoPoco/Configuration/EngineConfigurationFactory.cs
@@ -12,6 +12,31 @@ namespace AutoPoco.Configuration
         {
             EngineConfiguration configuration = new EngineConfiguration();
 
+            FindAndRegisterAllTypes(configurationProvider, configuration);
+            
+            foreach (var type in configurationProvider.GetConfigurationTypes())
+            {
+                ApplyConventionsToType(conventionProvider, configuration, type);
+                ApplyConfigurationToTypeMembers(configuration, type);
+                ApplyConventionsToTypeMembers(conventionProvider, configuration, type);
+            }
+            return configuration;
+        }
+
+        public void ApplyConventionsToType(IEngineConventionProvider conventionProvider, IEngineConfiguration configuration, IEngineConfigurationTypeProvider type)
+        {
+            var configuredType = configuration.GetRegisteredType(type.GetConfigurationType());
+            conventionProvider.Find<ITypeConvention>()
+                .Select(t => (ITypeConvention)Activator.CreateInstance(t))
+                .ToList()
+                .ForEach(x =>
+                {
+                    x.Apply(new TypeConventionContext(configuredType));
+                });
+        }
+
+        public void FindAndRegisterAllTypes(IEngineConfigurationProvider configurationProvider, EngineConfiguration configuration)
+        {
             // Perform all type registration
             foreach (var type in configurationProvider.GetConfigurationTypes())
             {
@@ -23,73 +48,66 @@ namespace AutoPoco.Configuration
                 {
                     configuration.RegisterType(configType);
                     configuredType = configuration.GetRegisteredType(configType);
-
-                    // Apply Type Conventions
-                    conventionProvider.Find<ITypeConvention>()
-                        .Select(t => (ITypeConvention)Activator.CreateInstance(t))
-                        .ToList()
-                        .ForEach(x =>
-                        {
-                            x.Apply(new TypeConventionContext(configuredType));
-                        });
                 }
             }
-
-            // Go through all of the explicitly-registered types and do their members
-            foreach (var type in configurationProvider.GetConfigurationTypes())
-            {
-                var configuredType = configuration.GetRegisteredType(type.GetConfigurationType());
-                foreach (var member in type.GetConfigurationMembers())
-                {
-                    EngineTypeMember typeMember = member.GetConfigurationMember();
-
-                    // Register the member if necessary
-                    var configuredMember = configuredType.GetRegisteredMember(typeMember);
-                    if (configuredMember == null)
-                    {
-                        configuredType.RegisterMember(typeMember);
-                        configuredMember = configuredType.GetRegisteredMember(typeMember);
-                    }
-
-                    // Set the action on that member if a datasource has been set explicitly for this type
-                    var datasources = member.GetDatasources();
-                    if (datasources.Count() > 0)
-                    {
-                        configuredMember.SetDatasources(datasources);
-                    }
-                }
-
-                // Apply Member Conventions to all convention configured members
-                foreach (var member in configuredType.GetRegisteredMembers())
-                {
-                    // If it's already been set, then ignore it
-                    if (member.GetDatasources().FirstOrDefault() != null) { continue; }
-
-                    // Apply the appropriate conventions
-                    if (member.Member.IsField)
-                    {
-                        conventionProvider.Find<ITypeFieldConvention>()
-                        .Select(t => (ITypeFieldConvention)Activator.CreateInstance(t))
-                        .ToList()
-                        .ForEach(x =>
-                        {
-                            x.Apply(new TypeFieldConventionContext(configuration, member));
-                        });
-                    }
-                    if (member.Member.IsProperty)
-                    {
-                        conventionProvider.Find<ITypePropertyConvention>()
-                        .Select(t => (ITypePropertyConvention)Activator.CreateInstance(t))
-                        .ToList()
-                        .ForEach(x =>
-                        {
-                            x.Apply(new TypePropertyConventionContext(configuration, member));
-                        });
-                    }
-                }
-
-            }
-            return configuration;
         }
+
+        public void ApplyConventionsToTypeMembers(IEngineConventionProvider conventionProvider, EngineConfiguration configuration, IEngineConfigurationTypeProvider type)
+        {
+            // Apply Member Conventions to all convention configured members
+            var configuredType = configuration.GetRegisteredType(type.GetConfigurationType());
+            foreach (var member in configuredType.GetRegisteredMembers())
+            {
+                // If it's already been set, then ignore it
+                if (member.GetDatasources().FirstOrDefault() != null) { continue; }
+
+                // Apply the appropriate conventions
+                if (member.Member.IsField)
+                {
+                    conventionProvider.Find<ITypeFieldConvention>()
+                    .Select(t => (ITypeFieldConvention)Activator.CreateInstance(t))
+                    .ToList()
+                    .ForEach(x =>
+                    {
+                        x.Apply(new TypeFieldConventionContext(configuration, member));
+                    });
+                }
+                if (member.Member.IsProperty)
+                {
+                    conventionProvider.Find<ITypePropertyConvention>()
+                    .Select(t => (ITypePropertyConvention)Activator.CreateInstance(t))
+                    .ToList()
+                    .ForEach(x =>
+                    {
+                        x.Apply(new TypePropertyConventionContext(configuration, member));
+                    });
+                }
+            }
+        }
+
+        public void ApplyConfigurationToTypeMembers(EngineConfiguration configuration, IEngineConfigurationTypeProvider type)
+        {
+            var configuredType = configuration.GetRegisteredType(type.GetConfigurationType());
+            foreach (var member in type.GetConfigurationMembers())
+            {
+                EngineTypeMember typeMember = member.GetConfigurationMember();
+
+                // Register the member if necessary
+                var configuredMember = configuredType.GetRegisteredMember(typeMember);
+                if (configuredMember == null)
+                {
+                    configuredType.RegisterMember(typeMember);
+                    configuredMember = configuredType.GetRegisteredMember(typeMember);
+                }
+
+                // Set the action on that member if a datasource has been set explicitly for this type
+                var datasources = member.GetDatasources();
+                if (datasources.Count() > 0)
+                {
+                    configuredMember.SetDatasources(datasources);
+                }
+            }
+        }
+
     }
 }
