1ea011f25678ccdd705dca2eb6068aa0397fd4a3
Khalid Abuhakmeh <birdchest@gmail.com>
2010-04-13 09:36:07 -0400

    added U.S. States, Countries, Credit Cards, and Lorem Ipsum

--- AutoPoco/DataSources/DefaultFloatSource.cs
+++ AutoPoco/DataSources/DefaultFloatSource.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using AutoPoco.Engine;
@@ -14,67 +12,4 @@ namespace AutoPoco.DataSources
             return 0;
         }
     }
-
-    public class DateOfBirthSource : DatasourceBase<DateTime>
-    {
-        private readonly Random _random;
-        private readonly int _yearsMax;
-        private readonly int _yearsMin;
-
-        public DateOfBirthSource()
-            :this(16, 59)
-        {}
-
-        public DateOfBirthSource(int min, int max)
-        {
-            _random = new Random();
-            _yearsMax = max;
-            _yearsMin = min;
-        }
-
-        public override DateTime Next(IGenerationSession session)
-        {
-            return DateTime.Now.AddYears(-_random.Next(_yearsMin, _yearsMax));
-        }
-    }
-
-    public class GuidSource : DatasourceBase<Guid>
-    {
-        #region Overrides of DatasourceBase<Guid>
-
-        public override Guid Next(IGenerationSession session)
-        {
-            return Guid.NewGuid();
-        }
-
-        #endregion
-    }
-
-    public class ColorSource : DatasourceBase<Color>
-    {
-        private readonly int _max;
-        private readonly Random _random;
-        private readonly int _min;
-
-        public ColorSource()
-        {
-            _random = new Random();
-            _min = 0;
-            _max = 255;
-        }
-
-        #region Overrides of DatasourceBase<Color>
-
-        public override Color Next(IGenerationSession session)
-        {
-            return Color.FromArgb(
-                    _random.Next(_min, _max), 
-                    _random.Next(_min, _max), 
-                    _random.Next(_min, _max)
-                );
-        }
-
-        #endregion
-    }
-
 }
