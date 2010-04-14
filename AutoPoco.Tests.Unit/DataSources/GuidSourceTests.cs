0f203a62533af501ccbc5a0a4895bd236e1d7e11
rashton <rashton@localhost>
2010-04-14 22:08:14 +0100

    Modified the guid data source to return duff, but repeatable guids

--- AutoPoco.Tests.Unit/DataSources/GuidSourceTests.cs
+++ AutoPoco.Tests.Unit/DataSources/GuidSourceTests.cs
@@ -14,5 +14,27 @@ namespace AutoPoco.Tests.Unit.DataSources
             var value = source.Next(null);
             Assert.IsFalse(value.CompareTo(Guid.Empty) == 0);
         }
+
+
+        [Test]
+        public void Next_WithTwoSources_Returns_RepeatedGuids()
+        {
+            GuidSource source1 = new GuidSource();
+            var value1 = source1.Next(null);
+
+            GuidSource source2 = new GuidSource();
+            var value2 = source2.Next(null);
+
+            Assert.AreEqual(value1, value2);
+        }
+
+        [Test]
+        public void Next_WithOneSource_ReturnsDifferentGuids()
+        {
+            GuidSource source = new GuidSource();
+            var value1 = source.Next(null);
+            var value2 = source.Next(null);
+            Assert.AreNotEqual(value1, value2);
+        }
     }
 }
