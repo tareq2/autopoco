e9e9b02fc811b0b066d3680ef77a6b31d774cf2e
rashton <rashton@localhost>
2010-04-14 18:32:33 +0100

    Added a full description of the inheritance regression problem

--- AutoPoco.Tests.Unit/Regression/WhenStronglyTypedVirtualPropertyRequested.cs
+++ AutoPoco.Tests.Unit/Regression/WhenStronglyTypedVirtualPropertyRequested.cs
@@ -19,6 +19,15 @@ namespace AutoPoco.Tests.Unit.Regression
             mMember = (EngineTypePropertyMember)ReflectionHelper.GetMember<DerivedClass>(x => x.SomeProperty);
         }
 
+        /// <summary>
+        /// This caused some major headaches with inheritance, as type conventions request members as a string
+        /// and configuration requests via expression.
+        /// MemberExpressions tend to return the base-most instance of virtual properties
+        /// GetProperty tends to return the most-derived instance of a virtual property
+        /// This causes some disparity when doing comparisons further on in configuration and is therefore undesirable behaviour
+        /// Rather than try to fix the problem at the point at which it becomes an issue, I decided to fix this core helper method
+        /// We'll see how that fares along with all the other work-arounds I've had to employ to get the inheritance/interface stuff going properly
+        /// </summary>
         [Test]
         public void RequestedProperty_IsOverriddenProperty()
         {
