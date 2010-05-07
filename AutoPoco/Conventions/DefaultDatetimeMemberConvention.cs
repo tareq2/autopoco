3096547308aab065b3c4518f2fe4d65289af2f00
rashton <rashton@localhost>
2010-05-07 16:22:18 +0100

    Fixed a couple of issues whereby multiple types being registered using diffeernt methods could cause actions to fail

--- AutoPoco/Conventions/DefaultDatetimeMemberConvention.cs
+++ AutoPoco/Conventions/DefaultDatetimeMemberConvention.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using AutoPoco.Configuration;
+using AutoPoco.DataSources;
 
 namespace AutoPoco.Conventions
 {
@@ -21,6 +22,20 @@ namespace AutoPoco.Conventions
         public void SpecifyRequirements(ITypeMemberConventionRequirements requirements)
         {
             requirements.Type(x => x == typeof(DateTime));
+        }
+    }
+
+    public class EmailAddressPropertyConvention : ITypePropertyConvention
+    {
+        public void Apply(ITypePropertyConventionContext context)
+        {
+            context.SetSource<EmailAddressSource>();
+        }
+
+        public void SpecifyRequirements(ITypeMemberConventionRequirements requirements)
+        {
+            requirements.Name(x => String.Compare(x, "EmailAddress", true) == 0);
+            requirements.Type(x => x == typeof(String));
         }
     }
 }
