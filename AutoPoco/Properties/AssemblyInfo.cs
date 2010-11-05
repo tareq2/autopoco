a486400350d43f6a3d857a3d31046bfcc51a6608
rashton <rashton@localhost>
2010-11-05 10:59:30 +0000

    Updated assembly info ready for packaging

--- AutoPoco/Properties/AssemblyInfo.cs
+++ AutoPoco/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pogogen")]
+[assembly: AssemblyTitle("AutoPoco")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Pogogen")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("codeofrob")]
+[assembly: AssemblyProduct("AutoPoco")]
+[assembly: AssemblyCopyright("Copyright © Rob Ashton 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
